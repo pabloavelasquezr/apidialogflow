85b88a5707537330c23223e4419b6ac4be6a0cbb
Pablo Velasquez <pabloavelasquez@gmail.com>
2024-08-31 14:42:43 -0500

    add front

---
+++ resources/views/home.blade.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+    <title>Laravel ApiDialogFlow</title>
+  </head>
+  <body>   
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="sidebar-container">
+      <div class="sidebar-box-container">
+        <div class="sidebar-logo">
+          <h4><span class="text-orange">Laravel</span> <span class="text-violet">Api</span><span class="text-purple">DialogFlow</span></h4>
+        </div>
+        <ul class="sidebar-navigation">
+          <li>
+            <a href="#scrollspyHeading1">
+              <i class="fa fa-home" aria-hidden="true"></i>Homepage
+            </a>
+          </li>
+          <li>
+            <a href="#scrollspyHeading2">
+              <i class="fa fa-tachometer" aria-hidden="true"></i>Who are we?
+            </a>
+          </li>
+          <li>
+            <a href="#scrollspyHeading3">
+              <i class="fa fa-tachometer" aria-hidden="true"></i>We are global
+            </a>
+          </li>
+          <li>
+            <a href="#scrollspyHeading4">
+              <i class="fa fa-tachometer" aria-hidden="true"></i>Join us
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    
+    <div class="content-container">
+    
+    
+        <!-- Main component for a primary marketing message or call to action -->
+        <div class="contenedor" id="scrollspyHeading1">
+          <div class="screen screen1">
+            <div class="screen1-row1"></div>
+
+            <div class="screen1-row2">
+              <div class="screen1-centralbox">
+                <div class="screen1-centralbox-textbox">
+                  <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<span> Sed euismod</span></h2>
+                </div>
+                <div class="screen1-centralbox-logobox">
+                </div>
+              </div>
+            </div>
+
+            <div class="screen1-row3">
+              <div class="screen1-url">
+                <a href="http://apidialogflow.synergize.co" target="_blank">
+                apidialogflow.synergize.co
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+        <div class="contenedor" id="scrollspyHeading2">
+          <div class="screen screen2">
+
+            <div class="screen2-image"></div>
+            <div class="screen2-text"><h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod</h2></div>
+            <div class="screen2-boxes">
+                <div class="screen2-boxes-box screen2-boxes-box-img-rocket">
+                  <img>
+                  <h3>Our mission</h3>
+                  <p>Sed euismod facilisis felis, id ultrices nunc. Sed nec nunc id nunc tincidunt lacinia. Nulla facilisi. Sed vitae nunc sit amet nunc lacinia lacinia.
+                  </p>
+                </div>
+                <div class="screen2-boxes-box screen2-boxes-box-img-eye">
+                  <img>
+                  <h3>Our vision</h3>
+                  <p>ulla urna odio, tincidunt sed commodo id, porta vel arcu. Mauris imperdiet risus quis eros bibendum auctor.
+                </div>
+            </div>
+            
+          </div>
+        </div>
+
+        <div class="contenedor" id="scrollspyHeading3">
+          <div class="screen screen3">
+
+            <div class="screen3-row1">
+              <div class="screen3-row1-textleft">
+                <h2>In sodales augue non eleifend eleifend.</h2>
+                <p>Curabitur vel turpis sit amet lorem euismod hendrerit vel fringilla augue. Proin a sem erat. Aliquam erat volutpat. <b> ante ipsum primis in faucibus</b></p>
+                <p>Learn more about us here</p>
+                <button>Lorem ipsum</button>
+              </div>
+              <div class="screen3-row1-imageright"><!-- <img> --></div>
+            </div>
+            
+            <div class="screen3-row2">
+              <div class="screen3-row2-top">
+                <div class="screen3-row2-top-title">
+                    <h1>Lorem ipsum dolor sit</h1>
+                </div>
+                <div class="screen3-row2-top-textbox">
+                  <div class="screen3-row2-top-textbox-orange"><b>+ 600</b>&nbsp; Suscipit</div>
+                  <div class="screen3-row2-top-textbox-violet"><b>30</b>&nbsp; Semper</div>
+                  <div class="screen3-row2-top-textbox-purple"><b>54</b>&nbsp; Tinibus</div>
+                </div>
+              </div>
+
+              <div class="screen3-row2-bottom">
+                <div class="screen3-row2-bottom-boxes">
+
+                  <div class="screen3-row2-bottom-boxes-orange">
+                    <p>Bogotá, Quito, Lima, Mexico City, Panama City, San José, Santiago, Buenos Aires, Washington D.C., Abu Dhabi,
+                      Brasília, Santo Domingo, Dublin, Ottawa, Madrid, Lisbon, Beijing, Rome.</p>
+                  </div>
+                  <div class="screen3-row2-bottom-boxes-violet">
+                    <p>Washington D.C., Bogotá, Lima, Brasília, Santiago, Mexico City, Caracas.</p>
+                  </div>
+                  <div class="screen3-row2-bottom-boxes-purple">
+                    <p>Colombia, Sudáfrica, Filipinas, Chile, México, Singapur.</p>
+                  </div>
+
+                </div>
+
+                <div class="screen3-row2-bottom-map"></div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div class="contenedor" id="scrollspyHeading4">
+          <div class="screen screen4">
+
+
+            <div class="screen4-row1">
+              <div class="screen4-logo"><img></div>
+            </div>
+            
+            <div class="screen4-row2"></div>
+
+            <div class="screen4-row3">
+              <div class="screen4-box">
+                <div class="screen4-box-title">
+                  <h1>Nulla et tellus vitae sapien!</h1>
+                </div>
+                <div class="screen4-box-info">
+                  <h4>Maecenas molestie imperdiet sapien dolor quis arcu.</h4>
+                  <p>Duis vitae nisi at nibh consequat gravida. Nunc a bibendum massa.</p>
+                  <div class="screen4-box-info-social">
+                    <a href="#"><img src="https://cdn-icons-png.flaticon.com/128/6422/6422199.png"></a>
+                    <a href="#"><img src="https://cdn-icons-png.flaticon.com/128/6422/6422200.png"></a>
+                    <a href="#"><img src="https://cdn-icons-png.flaticon.com/128/6422/6422207.png"></a>
+                    <a href="#"><img src="https://cdn-icons-png.flaticon.com/128/6422/6422215.png"></a>
+                    <a href="#"><img src="https://cdn-icons-png.flaticon.com/128/6422/6422202.png"></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="screen4-row4">
+              <div class="screen4-url">
+                <a href="http://apidialogflow.synergize.co" target="_blank">
+                  apidialogflow.synergize.co
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
